17d46559576a9e947b7a43a638c95a85e1bfe49f
MatFreitas <matheusfs2@al.insper.edu.br>
2020-05-05 02:53:09 -0300

    Ícone de envio, qnd clica abre contatos (ainda ñ pega info para enviar o SMS)

--- app/src/main/java/br/pro/hashi/ensino/desagil/projeto1/MainActivity.java
+++ app/src/main/java/br/pro/hashi/ensino/desagil/projeto1/MainActivity.java
@@ -1,11 +1,15 @@
 package br.pro.hashi.ensino.desagil.projeto1;
 
 import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
+import android.provider.ContactsContract;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
+import android.widget.ImageButton;
+import android.widget.ImageView;
 import android.widget.TextView;
 import android.widget.Toast;
 
@@ -14,6 +18,27 @@ import androidx.appcompat.app.AppCompatActivity;
 
 public class MainActivity extends AppCompatActivity {
     String content;
+    static final int REQUEST_SELECT_CONTACT = 1;
+
+    public void selectContact() {
+        Intent intent = new Intent(Intent.ACTION_PICK);
+        intent.setType(ContactsContract.Contacts.CONTENT_TYPE);
+        if (intent.resolveActivity(getPackageManager()) != null) {
+            startActivityForResult(intent, REQUEST_SELECT_CONTACT);
+        }
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if (requestCode == REQUEST_SELECT_CONTACT && resultCode == RESULT_OK) {
+            Uri contactUri = data.getData();
+            System.out.println("00000000000000000" + contactUri.getHost());
+        }
+    }
+
+
+
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -23,7 +48,7 @@ public class MainActivity extends AppCompatActivity {
         Button buttonMessage2 = findViewById(R.id.button_message2);
         Button buttonMessage3 = findViewById(R.id.button_message3);
         Button buttonMessage4 = findViewById(R.id.button_message4);
-        Button buttonMessageSend = findViewById(R.id.export_message);
+        ImageView buttonMessageSend = findViewById(R.id.export_message);
         TextView textMessage = findViewById(R.id.text_message);
 
         buttonMessage1.setOnClickListener((view -> {
@@ -50,7 +75,8 @@ public class MainActivity extends AppCompatActivity {
             if (textMessage.getText().toString().equals("")) {
                 Toast.makeText(this,"No text to export", Toast.LENGTH_SHORT).show();
             } else {
-                exportMessage(textMessage.getText());
+                // exportMessage(textMessage.getText());
+                selectContact();
             }
         }));
     }
